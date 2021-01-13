6fe80c8985e67088ecf6884c605ab68b0b7842e4
Poko56 <claudioropez.k@gmail.com>
2021-01-13 15:20:24 +0900

    fix(header): warningへの対応

--- src/components/Header.component.tsx
+++ src/components/Header.component.tsx
@@ -96,7 +96,7 @@ const Header: React.FC = () => {
                   aria-labelledby="user-menu"
                 >
                   {UserMenuData.map((item: LinkProps) => (
-                    <Link href={item.href}>
+                    <Link href={item.href} key={item.href}>
                       <a
                         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                         role="menuitem"
@@ -194,9 +194,9 @@ const Header: React.FC = () => {
                 aria-hidden="true"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                 ></path>
               </svg>
@@ -204,7 +204,7 @@ const Header: React.FC = () => {
           </div>
           <div className="mt-3 space-y-1">
             {UserMenuData.map((item: LinkProps) => (
-              <Link href={item.href}>
+              <Link href={item.href} key={item.name}>
                 <a
                   className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
                   role="menuitem"
