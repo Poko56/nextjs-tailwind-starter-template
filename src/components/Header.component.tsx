7365014350ce99b2427cede259a78f6d154aa751
Poko56 <claudioropez.k@gmail.com>
2021-01-13 15:14:25 +0900

    feat(header): スマホメニューやユーザー用のメニューを追加

--- src/components/Header.component.tsx
+++ src/components/Header.component.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import Link from "next/link"
 
 type LinkProps = {
@@ -6,13 +6,30 @@ type LinkProps = {
   href: string
 }
 
-const linkData = [
+const GlobalMenuData = [
   { name: "Home", href: "/" },
   { name: "About", href: "/about/" },
 ]
 
-const Header: React.FC = () => (
-  <header>
+const UserMenuData = [
+  { name: "Your Profile", href: "/" },
+  { name: "Settings", href: "/" },
+  { name: "Sign Out", href: "/" },
+]
+
+const Header: React.FC = () => {
+  const [isSPMenuOpen, setSPMenuOpen] = useState(false)
+  const [isUserMenuOpen, setUserMenuOpen] = useState(false)
+
+  const handleSPMenuClick = () => {
+    setSPMenuOpen(!isSPMenuOpen)
+  }
+
+  const handleUserMenuClick = (): void => {
+    setUserMenuOpen(!isUserMenuOpen)
+  }
+
+  return (
     <nav className="bg-white border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
@@ -25,7 +42,7 @@ const Header: React.FC = () => (
               </h1>
             </div>
             <div className="hidden sm:-my-px sm:ml-12 sm:flex sm:space-x-8">
-              {linkData.map((item: LinkProps) => (
+              {GlobalMenuData.map((item: LinkProps) => (
                 <Link href={item.href} key={item.href}>
                   <a className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
                     {item.name}
@@ -34,10 +51,174 @@ const Header: React.FC = () => (
               ))}
             </div>
           </div>
+          <div className="hidden sm:ml-6 sm:flex sm:items-center">
+            <button className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <span className="sr-only">View notifications</span>
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />
+              </svg>
+            </button>
+
+            <div className="ml-3 relative">
+              <div>
+                <button
+                  className="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  id="user-menu"
+                  aria-haspopup="true"
+                  onClick={handleUserMenuClick}
+                >
+                  <span className="sr-only">Open user menu</span>
+                  <img
+                    className="h-8 w-8 rounded-full"
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                </button>
+              </div>
+
+              {isUserMenuOpen && (
+                <div
+                  className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="user-menu"
+                >
+                  {UserMenuData.map((item: LinkProps) => (
+                    <Link href={item.href}>
+                      <a
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                        role="menuitem"
+                      >
+                        {item.name}
+                      </a>
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="-mr-2 flex items-center sm:hidden">
+            <button
+              className="bg-white inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={handleSPMenuClick}
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="block h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+
+              <svg
+                className="hidden h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className={`${isSPMenuOpen ? "" : "hidden"} sm:hidden`}>
+        <div className="pt-2 pb-3 space-y-1">
+          {GlobalMenuData.map((item: LinkProps) => (
+            <Link href={item.href} key={item.href}>
+              <a
+                className="border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                onClick={() => setSPMenuOpen(false)}
+              >
+                {item.name}
+              </a>
+            </Link>
+          ))}
+        </div>
+
+        <div className="pt-4 pb-3 border-t border-gray-200">
+          <div className="flex items-center px-4">
+            <div className="flex-shrink-0">
+              <img
+                className="h-10 w-10 rounded-full"
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
+                alt=""
+              />
+            </div>
+            <div className="ml-3">
+              <div className="text-base font-medium text-gray-800">
+                Tom Cook
+              </div>
+              <div className="text-sm font-medium text-gray-500">
+                tom@example.com
+              </div>
+            </div>
+            <button className="ml-auto bg-white flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <span className="sr-only">View notifications</span>
+              <svg
+                className="h-6 w-6"
+                x-description="Heroicon name: bell"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <div className="mt-3 space-y-1">
+            {UserMenuData.map((item: LinkProps) => (
+              <Link href={item.href}>
+                <a
+                  className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
+                  role="menuitem"
+                  onClick={() => setSPMenuOpen(false)}
+                >
+                  {item.name}
+                </a>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </nav>
-  </header>
-)
+  )
+}
 
 export default Header
