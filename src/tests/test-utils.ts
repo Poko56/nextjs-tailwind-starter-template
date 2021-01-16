3c9544077ba7210b9e0a01a8abe57ae1046a6911
Poko56 <claudioropez.k@gmail.com>
2021-01-16 10:18:54 +0900

    test: 設定用のコードを追加

---
+++ src/tests/test-utils.ts
@@ -0,0 +1,13 @@
+import * as nextRouter from 'next/router'
+
+nextRouter.useRouter = () => ({
+  route: '/',
+  pathname: '/',
+})
+
+nextRouter.router = {
+  push: () => {},
+  prefetch: () => new Promise((resolve, reject) => {}),
+}
+
+export default nextRouter
